734e82cad965a22d5b9ac0f333550127e3ec2a2e
Sergio99778 <sergio4brandon@gmail.com>
2022-01-17 23:10:23 -0600

    Adding Form to App

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import "../bootstrap.min.css";
 
 const Form = () => {
-  return <p>Form</p>;
+  return (
+    <form className="col-12">
+      <fieldset className="text-center">
+        <legend>Search by ingredient or category</legend>
+      </fieldset>
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            type="text"
+            name="name"
+            className="form-control"
+            placeholder="Search by ingredient"
+          />
+        </div>
+        <div className="col-md-4">
+          <select name="category" id="" className="form-control">
+            <option value="">-- Select Category</option>
+            <option value="other">other</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <button type="submit" className="btn btn-primary btn-block">
+            Search
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default Form;
