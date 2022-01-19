34ccb6ecf0513cc971bcca198a5ef93875cdc498
Sergio99778 <sergio4brandon@gmail.com>
2022-01-19 03:18:34 -0600

    Adding modal to Recipe and @material-ui/core

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,9 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 //Context
 import { ModalContext } from "../context/ModalContext";
 
+//Material UI modal
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 450,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+//End Material UI modal
+
 const Recipe = ({ recipe }) => {
+  //config Material UI modal
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   //State from context
   const { setIdRecipe } = useContext(ModalContext);
 
@@ -18,10 +55,22 @@ const Recipe = ({ recipe }) => {
             className="btn btn-primary btn-block"
             onClick={() => {
               setIdRecipe(recipe.idDrink);
+              handleOpen();
             }}
           >
             Show recipe
           </button>
+          <Modal
+            open={open}
+            onClose={() => {
+              handleClose();
+              setIdRecipe(null);
+            }}
+          >
+            <div style={modalStyle} className={classes.paper}>
+              <h1>From modal</h1>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
